9873115fab8003aa187665147da759533d4cf433
Frozennfishh <leonard.ong0108@gmail.com>
2025-03-12 22:08:29 +0800

    Add Testcase for StartDateTest

--- src/test/java/seedu/address/model/reservation/StartDateTest.java
+++ src/test/java/seedu/address/model/reservation/StartDateTest.java
@@ -1,4 +1,26 @@
 package seedu.address.model.reservation;
 
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class StartDateTest {
+    @Test
+    public void constructor_invalidDate_throwsIllegalArgumentException() {
+        // Invalid date formats
+        String[] invalidDates = {
+            "32/01/2024", // Invalid day
+            "31/13/2024", // Invalid month
+            "1/1/2024", // Missing leading zeros
+            "29/02/2023", // Invalid leap year date
+            "31/04/2024", // April only has 30 days
+            "00/12/2024", // 00 is not a valid day
+            "12/00/2024", // 00 is not a valid month
+            "" // Empty string
+        };
+
+        for (String date : invalidDates) {
+            assertThrows(IllegalArgumentException.class, () -> new StartDate(date));
+        }
+    }
 }
