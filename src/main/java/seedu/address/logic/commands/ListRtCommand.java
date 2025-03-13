802f43f71a0bbd9b53d2b452cc94e07acea3b6eb
Jasonzhou97 <cheetahjason168@gmail.com>
2025-03-13 21:03:15 +0800

    Edit style

--- src/main/java/seedu/address/logic/commands/ListRtCommand.java
+++ src/main/java/seedu/address/logic/commands/ListRtCommand.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
-import seedu.address.model.Model;
-
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RESERVATIONS;
 
-public class ListRtCommand extends Command{
+import seedu.address.model.Model;
+
+/**
+ * Lists all reservations scheduled for today.
+ */
+public class ListRtCommand extends Command {
     public static final String COMMAND_WORD = "listrt";
     public static final String MESSAGE_SUCCESS = "Listed all reservations for today";
     @Override
