0641d04ed39b0f1625b3ce9f6acbde66c1d95743
Jasonzhou97 <cheetahjason168@gmail.com>
2025-03-17 11:27:09 +0800

    Delete unused lines

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -11,8 +11,6 @@ import seedu.address.MainApp;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.Logic;
-import seedu.address.logic.commands.ListRtCommand;
-import seedu.address.logic.commands.ListRtCommand;
 /**
  * The manager of the UI component.
  */
@@ -44,7 +42,6 @@ public class UiManager implements Ui {
             mainWindow = new MainWindow(primaryStage, logic);
             mainWindow.show(); //This should be called before creating other UI parts
             mainWindow.fillInnerParts();
-            ListRtCommand listrt = new ListRtCommand();
             logic.execute("listrt");
 
         } catch (Throwable e) {
