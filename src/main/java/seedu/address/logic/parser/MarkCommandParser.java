dfefda04de76622abe886986f62eaf5b84373180
Jasonzhou97 <cheetahjason168@gmail.com>
2025-03-19 10:55:16 +0800

    Fix id error

--- src/main/java/seedu/address/logic/parser/MarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MarkCommandParser.java
@@ -7,6 +7,8 @@ import seedu.address.model.reservation.Identification;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.reservation.Phone;
+import seedu.address.model.reservation.StartDate;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
@@ -15,6 +17,7 @@ public class MarkCommandParser implements Parser<MarkCommand> {
     @Override
     public MarkCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
+        String[] words = trimmedArgs.split(" ");
         //throws parseException when the args is empty
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
@@ -23,7 +26,7 @@ public class MarkCommandParser implements Parser<MarkCommand> {
 
         try {
             //if the bookingId is invalid form, may throw error and show markCommand.message_usage
-            Identification bookingId = new Identification(trimmedArgs);
+            Identification bookingId = new Identification(new StartDate(words[0]), new Phone(words[1]));
             return new MarkCommand(bookingId);
         } catch (IllegalArgumentException e) {
             throw new ParseException(
