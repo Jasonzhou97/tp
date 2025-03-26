d0b5bdc03cb2fb5d476fd6e08dbab2d1708bb6df
caroline1233456 <caroline-qingru.yang@outlook.com>
2025-03-26 18:33:54 +0800

    Add markCommand test cases

--- src/test/java/seedu/address/logic/commands/MarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MarkCommandTest.java
@@ -1,2 +1,63 @@
-package seedu.address.logic.commands;public class MarkCommandTest {
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.reservation.Identification;
+import seedu.address.model.reservation.Reservation;
+import seedu.address.model.reservation.StartTime;
+import seedu.address.testutil.ReservationBuilder;
+import seedu.address.logic.commands.CommandTestUtil;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.MarkCommand.MESSAGE_MARK_RESERVATION_SUCCESS;
+import static seedu.address.model.reservation.Identification.getReservationUsingId;
+import static seedu.address.testutil.TypicalReservations.getTypicalAddressBook;
+
+public class MarkCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    //partition: id that exist and id that does not exist
+
+    @Test
+    public void execute_validCommandAndId_success(){
+        Reservation reservationToMark = model.getFilteredReservationList().get(0);
+        Identification id = new Identification(reservationToMark.getDate(), reservationToMark.getPhone(),
+                reservationToMark.getTime());
+
+        MarkCommand markCommand = new MarkCommand(id);
+
+        String expectedMessage = String.format(MESSAGE_MARK_RESERVATION_SUCCESS,
+                Messages.format(reservationToMark));
+        Reservation reservationMarked = new ReservationBuilder(reservationToMark)
+                .withPaymentStatus(true).build();
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.setReservation(reservationToMark, reservationMarked);
+        assertCommandSuccess(markCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_validCommandAndNotExistId_fail(){
+        Reservation reservationToMark = model.getFilteredReservationList().get(0);
+        Identification id = new Identification(reservationToMark.getDate(), reservationToMark.getPhone(),
+                new StartTime("2359"));
+
+        MarkCommand markCommand = new MarkCommand(id);
+
+        String expectedMessage = "Input reservation id does not exist.";
+
+        assertCommandFailure(markCommand, model, expectedMessage);
+    }
+
+
+
+
+
+
+
 }
