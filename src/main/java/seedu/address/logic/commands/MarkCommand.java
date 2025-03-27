453349e35da15268d562cf9527babbe5abd03fe6
Frozennfishh <93506857+Frozennfishh@users.noreply.github.com>
2025-03-27 12:09:15 +0800

    Fix Checkstyle Violations
    Excludes public access violation as it breaks programme

--- src/main/java/seedu/address/logic/commands/MarkCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkCommand.java
@@ -52,8 +52,8 @@ public class MarkCommand extends Command {
     }
 
     @Override
-    public boolean equals(Object other){
-        if (! (other instanceof MarkCommand)) {
+    public boolean equals(Object other) {
+        if (!(other instanceof MarkCommand)) {
             return false;
         }
         // of type MarkCommand:
