2a64a3f8ed6d9fb44534176cdbfbcb5968e2f1f1
Jasonzhou97 <cheetahjason168@gmail.com>
2025-03-27 23:20:50 +0800

    Edit style

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -192,7 +192,7 @@ public class AddCommandTest {
             throw new AssertionError("This method should not be called.");
         }
         @Override
-        public void updatePersonsListAfterDelete(Reservation deletedReservation){
+        public void updatePersonsListAfterDelete(Reservation deletedReservation) {
             throw new AssertionError("This method should not be called.");
         };
         @Override
