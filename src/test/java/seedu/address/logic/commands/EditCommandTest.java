72e56ab989afd1f077ab63c9cbdddd2477459916
CVincent0907 <vincent045731@gmail.com>
2025-04-01 15:33:35 +0800

    Update styling

--- src/test/java/seedu/address/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditCommandTest.java
@@ -24,79 +24,78 @@ import seedu.address.testutil.ReservationBuilder;
 public class EditCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    @Test
+    public void execute_allFieldsSpecifiedUnfilteredList_success() {
+        Reservation editedReservation = ALICE;
+        EditCommand.EditReservationDescriptor descriptor = new EditReservationDescriptorBuilder(
+            editedReservation).build();
+        EditCommand editCommand = new EditCommand(editedReservation.getId(), descriptor);
 
-        @Test
-        public void execute_allFieldsSpecifiedUnfilteredList_success() {
-            Reservation editedReservation = ALICE;
-            EditCommand.EditReservationDescriptor descriptor = new EditReservationDescriptorBuilder(
-                    editedReservation).build();
-            EditCommand editCommand = new EditCommand(editedReservation.getId(),descriptor);
+        String expectedMessage = String.format(
+            EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
+            Messages.format(editedReservation));
 
-            String expectedMessage = String.format(
-                    EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
-                    Messages.format(editedReservation));
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setReservation(model.getFilteredReservationList().get(0), editedReservation);
 
-            Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-            expectedModel.setReservation(model.getFilteredReservationList().get(0), editedReservation);
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 
-            assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-        }
+    @Test
+    public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Reservation lastReservation = model.getFilteredReservationList().get(1); // BENSON
 
-        @Test
-        public void execute_someFieldsSpecifiedUnfilteredList_success() {
-            Reservation lastReservation = model.getFilteredReservationList().get(1); // BENSON
+        ReservationBuilder personInList = new ReservationBuilder(lastReservation);
+        Reservation editedReservation = personInList.withName(VALID_NAME_BOB).withDuration(VALID_DURATION_BOB)
+            .withTags(VALID_TAG_HUSBAND).build();
 
-            ReservationBuilder personInList = new ReservationBuilder(lastReservation);
-            Reservation editedReservation = personInList.withName(VALID_NAME_BOB).withDuration(VALID_DURATION_BOB)
-                    .withTags(VALID_TAG_HUSBAND).build();
-
-            EditCommand.EditReservationDescriptor descriptor =
-                    new EditReservationDescriptorBuilder().withName(VALID_NAME_BOB)
-                    .withDuration(VALID_DURATION_BOB).withTags(VALID_TAG_HUSBAND).build();
+        EditCommand.EditReservationDescriptor descriptor =
+            new EditReservationDescriptorBuilder().withName(VALID_NAME_BOB)
+            .withDuration(VALID_DURATION_BOB).withTags(VALID_TAG_HUSBAND).build();
 
 
-            EditCommand editCommand = new EditCommand(lastReservation.getId(), descriptor);
+        EditCommand editCommand = new EditCommand(lastReservation.getId(), descriptor);
 
-            String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
-                    Messages.format(editedReservation));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
+            Messages.format(editedReservation));
 
-            Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-            expectedModel.setReservation(lastReservation, editedReservation);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setReservation(lastReservation, editedReservation);
 
-            assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-        }
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 
-        @Test
-        public void execute_noFieldSpecifiedUnfilteredList_success() {
-            Reservation editedReservation = model.getFilteredReservationList().get(1);
-            EditCommand editCommand = new EditCommand(editedReservation.getId(),
-                    new EditCommand.EditReservationDescriptor());
+    @Test
+    public void execute_noFieldSpecifiedUnfilteredList_success() {
+        Reservation editedReservation = model.getFilteredReservationList().get(1);
+        EditCommand editCommand = new EditCommand(editedReservation.getId(),
+                new EditCommand.EditReservationDescriptor());
 
-            String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
-                    Messages.format(editedReservation));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
+                Messages.format(editedReservation));
 
-            Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
 
-            assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-        }
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 
-        @Test
-        public void execute_filteredList_success() {
-            //  showPersonAtIndex(model, INDEX_FIRST_PERSON);
+    @Test
+    public void execute_filteredList_success() {
+        //  showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-            Reservation reservationInFilteredList = model.getFilteredReservationList()
-                    .get(1);
-            Reservation editedReservation = new ReservationBuilder(reservationInFilteredList)
-                    .withName(VALID_NAME_BOB).build();
-            EditCommand editCommand = new EditCommand(reservationInFilteredList.getId(),
-                    new EditReservationDescriptorBuilder().withName(VALID_NAME_BOB).build());
+        Reservation reservationInFilteredList = model.getFilteredReservationList()
+                .get(1);
+        Reservation editedReservation = new ReservationBuilder(reservationInFilteredList)
+                .withName(VALID_NAME_BOB).build();
+        EditCommand editCommand = new EditCommand(reservationInFilteredList.getId(),
+                new EditReservationDescriptorBuilder().withName(VALID_NAME_BOB).build());
 
-            String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
-                    Messages.format(editedReservation));
+        String expectedMessage = String.format(EditCommand.MESSAGE_EDIT_RESERVATION_SUCCESS,
+                Messages.format(editedReservation));
 
-            Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-            expectedModel.setReservation(model.getFilteredReservationList().get(1), editedReservation);
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setReservation(model.getFilteredReservationList().get(1), editedReservation);
 
-            assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
-        }
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
+    }
 }
