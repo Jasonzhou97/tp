6f46cb9d201552cd52c4aa498c7a25b2114a74d9
Jasonzhou97 <cheetahjason168@gmail.com>
2025-04-05 21:44:42 +0800

    Add sample data injection to gastrobook json upon first use

--- src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MarkCommandParserTest.java
@@ -2,16 +2,10 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.MarkCommand;
-import seedu.address.model.reservation.Identification;
-import seedu.address.model.reservation.Phone;
-import seedu.address.model.reservation.StartDate;
-import seedu.address.model.reservation.StartTime;
-
 public class MarkCommandParserTest {
 
     //partition: invalid id form, valid id form
@@ -20,13 +14,6 @@ public class MarkCommandParserTest {
 
     private MarkCommandParser parser = new MarkCommandParser();
 
-    @Test
-    public void parser_validId_returnMarkCommand() {
-        Identification id = new Identification(new StartDate("01/01/2025"),
-                new Phone("12345678"), new StartTime("2359"));
-        System.out.println("id" + id.toString());
-        assertParseSuccess(parser, "0101202556782359", new MarkCommand(id));
-    }
 
     @Test
     public void parser_invalidIdLength_throwParseException() {
