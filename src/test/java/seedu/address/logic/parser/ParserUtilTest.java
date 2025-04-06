83a7b17861f41feb4c1c5d331656f2d189ea825a
Frozennfishh <leonard.ong0108@gmail.com>
2025-04-06 21:07:43 +0800

    Add ParserUtilTest

--- src/test/java/seedu/address/logic/parser/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/ParserUtilTest.java
@@ -1,148 +1,102 @@
-//package seedu.address.logic.parser;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
-//import static seedu.address.testutil.Assert.assertThrows;
-//import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.logic.parser.exceptions.ParseException;
-//import seedu.address.model.reservation.Name;
-//import seedu.address.model.reservation.Phone;
-//import seedu.address.model.tag.Tag;
-//
-//public class ParserUtilTest {
-//    private static final String INVALID_NAME = "R@chel";
-//    private static final String INVALID_PHONE = "+651234";
-//    private static final String INVALID_ADDRESS = " ";
-//    private static final String INVALID_EMAIL = "example.com";
-//    private static final String INVALID_TAG = "#friend";
-//
-//    private static final String VALID_NAME = "Rachel Walker";
-//    private static final String VALID_PHONE = "123456";
-//    private static final String VALID_ADDRESS = "123 Main Street #0505";
-//    private static final String VALID_EMAIL = "rachel@example.com";
-//    private static final String VALID_TAG_1 = "friend";
-//    private static final String VALID_TAG_2 = "neighbour";
-//
-//    private static final String WHITESPACE = " \t\r\n";
-//
-//    @Test
-//    public void parseIndex_invalidInput_throwsParseException() {
-//        assertThrows(ParseException.class, () -> ParserUtil.parseIndex("10 a"));
-//    }
-//
-//    @Test
-//    public void parseIndex_outOfRangeInput_throwsParseException() {
-//        assertThrows(ParseException.class, MESSAGE_INVALID_INDEX, ()
-//            -> ParserUtil.parseIndex(Long.toString(Integer.MAX_VALUE + 1)));
-//    }
-//
-//    @Test
-//    public void parseIndex_validInput_success() throws Exception {
-//        // No whitespaces
-//        assertEquals(INDEX_FIRST_PERSON, ParserUtil.parseIndex("1"));
-//
-//        // Leading and trailing whitespaces
-//        assertEquals(INDEX_FIRST_PERSON, ParserUtil.parseIndex("  1  "));
-//    }
-//
-//    @Test
-//    public void parseName_null_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> ParserUtil.parseName((String) null));
-//    }
-//
-//    @Test
-//    public void parseName_invalidValue_throwsParseException() {
-//        assertThrows(ParseException.class, () -> ParserUtil.parseName(INVALID_NAME));
-//    }
-//
-//    @Test
-//    public void parseName_validValueWithoutWhitespace_returnsName() throws Exception {
-//        Name expectedName = new Name(VALID_NAME);
-//        assertEquals(expectedName, ParserUtil.parseName(VALID_NAME));
-//    }
-//
-//    @Test
-//    public void parseName_validValueWithWhitespace_returnsTrimmedName() throws Exception {
-//        String nameWithWhitespace = WHITESPACE + VALID_NAME + WHITESPACE;
-//        Name expectedName = new Name(VALID_NAME);
-//        assertEquals(expectedName, ParserUtil.parseName(nameWithWhitespace));
-//    }
-//
-//    @Test
-//    public void parsePhone_null_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> ParserUtil.parsePhone((String) null));
-//    }
-//
-//    @Test
-//    public void parsePhone_invalidValue_throwsParseException() {
-//        assertThrows(ParseException.class, () -> ParserUtil.parsePhone(INVALID_PHONE));
-//    }
-//
-//    @Test
-//    public void parsePhone_validValueWithoutWhitespace_returnsPhone() throws Exception {
-//        Phone expectedPhone = new Phone(VALID_PHONE);
-//        assertEquals(expectedPhone, ParserUtil.parsePhone(VALID_PHONE));
-//    }
-//
-//    @Test
-//    public void parsePhone_validValueWithWhitespace_returnsTrimmedPhone() throws Exception {
-//        String phoneWithWhitespace = WHITESPACE + VALID_PHONE + WHITESPACE;
-//        Phone expectedPhone = new Phone(VALID_PHONE);
-//        assertEquals(expectedPhone, ParserUtil.parsePhone(phoneWithWhitespace));
-//    }
-//
-//    @Test
-//    public void parseTag_null_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> ParserUtil.parseTag(null));
-//    }
-//
-//    @Test
-//    public void parseTag_invalidValue_throwsParseException() {
-//        assertThrows(ParseException.class, () -> ParserUtil.parseTag(INVALID_TAG));
-//    }
-//
-//    @Test
-//    public void parseTag_validValueWithoutWhitespace_returnsTag() throws Exception {
-//        Tag expectedTag = new Tag(VALID_TAG_1);
-//        assertEquals(expectedTag, ParserUtil.parseTag(VALID_TAG_1));
-//    }
-//
-//    @Test
-//    public void parseTag_validValueWithWhitespace_returnsTrimmedTag() throws Exception {
-//        String tagWithWhitespace = WHITESPACE + VALID_TAG_1 + WHITESPACE;
-//        Tag expectedTag = new Tag(VALID_TAG_1);
-//        assertEquals(expectedTag, ParserUtil.parseTag(tagWithWhitespace));
-//    }
-//
-//    @Test
-//    public void parseTags_null_throwsNullPointerException() {
-//        assertThrows(NullPointerException.class, () -> ParserUtil.parseTags(null));
-//    }
-//
-//    @Test
-//    public void parseTags_collectionWithInvalidTags_throwsParseException() {
-//        assertThrows(ParseException.class, () -> ParserUtil.parseTags(Arrays.asList(VALID_TAG_1, INVALID_TAG)));
-//    }
-//
-//    @Test
-//    public void parseTags_emptyCollection_returnsEmptySet() throws Exception {
-//        assertTrue(ParserUtil.parseTags(Collections.emptyList()).isEmpty());
-//    }
-//
-//    @Test
-//    public void parseTags_collectionWithValidTags_returnsTagSet() throws Exception {
-//        Set<Tag> actualTagSet = ParserUtil.parseTags(Arrays.asList(VALID_TAG_1, VALID_TAG_2));
-//        Set<Tag> expectedTagSet = new HashSet<Tag>(Arrays.asList(new Tag(VALID_TAG_1), new Tag(VALID_TAG_2)));
-//
-//        assertEquals(expectedTagSet, actualTagSet);
-//    }
-//}
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.reservation.Duration;
+import seedu.address.model.reservation.Name;
+import seedu.address.model.reservation.Pax;
+import seedu.address.model.reservation.Phone;
+import seedu.address.model.reservation.StartTime;
+import seedu.address.model.reservation.Table;
+import seedu.address.model.tag.Tag;
+
+public class ParserUtilTest {
+
+    @Test
+    public void parseName_validValue_success() throws Exception {
+        String input = "John Doe";
+        Name expected = new Name(input);
+        assertEquals(expected, ParserUtil.parseName(input));
+    }
+
+    @Test
+    public void parseName_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseName(""));
+    }
+
+    @Test
+    public void parsePhone_validValue_success() throws Exception {
+        String input = "98765432";
+        Phone expected = new Phone(input);
+        assertEquals(expected, ParserUtil.parsePhone(input));
+    }
+
+    @Test
+    public void parsePhone_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parsePhone("123"));
+    }
+
+    @Test
+    public void parseDate_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseDate("01/05/2000"));
+    }
+
+    @Test
+    public void parseTime_validValue_success() throws Exception {
+        String input = "1400";
+        StartTime expected = new StartTime(input);
+        assertEquals(expected, ParserUtil.parseTime(input));
+    }
+
+    @Test
+    public void parseTime_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseTime("25:99"));
+    }
+
+    @Test
+    public void parseDuration_validValue_success() throws Exception {
+        String input = "1.5";
+        Duration expected = new Duration(input);
+        assertEquals(expected, ParserUtil.parseDuration(input));
+    }
+
+    @Test
+    public void parseDuration_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseDuration("13"));
+    }
+
+    @Test
+    public void parsePax_validValue_success() throws Exception {
+        String input = "5";
+        Pax expected = new Pax(input);
+        assertEquals(expected, ParserUtil.parsePax(input));
+    }
+
+    @Test
+    public void parsePax_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parsePax("10000"));
+    }
+
+    @Test
+    public void parseTable_validValue_success() throws Exception {
+        String input = "A1";
+        Table expected = new Table(input);
+        assertEquals(expected, ParserUtil.parseTable(input));
+    }
+
+    @Test
+    public void parseTable_invalidValue_throwsParseException() {
+        assertThrows(ParseException.class, () -> ParserUtil.parseTable("-1"));
+    }
+
+    @Test
+    public void parseTag_validValue_success() throws Exception {
+        String input = "Vegan";
+        Tag expected = new Tag(input);
+        assertEquals(expected, ParserUtil.parseTag(input));
+    }
+}
