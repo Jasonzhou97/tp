5cf2a2b1ef3f2274cc10d404a026678783466eab
Frozennfishh <leonard.ong0108@gmail.com>
2025-04-06 23:39:31 +0800

    Fix Name Length

--- src/main/java/seedu/address/model/reservation/Name.java
+++ src/main/java/seedu/address/model/reservation/Name.java
@@ -10,13 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain printable Unicode-Characters, and it should not be blank";
+            "Names should only contain printable Unicode-Characters between 1 - 30 characters,"
+                    + " and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^[\\P{Cntrl}]{1,50}$";
+    public static final String VALIDATION_REGEX = "^[\\P{Cntrl}]{1,30}$";
 
     private String fullName;
 
